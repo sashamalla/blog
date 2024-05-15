b00ff9bdb50543317f0df0dd7cb19835c1bcd1b9
Sasha Malla <sasha.malla@tum.de>
2024-05-15 11:21:37 +0200

    Updated logo

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Commahorror",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/logo.ong", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
