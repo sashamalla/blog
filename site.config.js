5612d0365934ac8564db3318a17dad7a85ec446d
Sasha Malla <sasha.malla@tum.de>
2024-05-15 20:26:30 +0200

    Updated formatting

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "CommaHorror!",
     image: "/logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "professional hatemonger",
-    bio: "This is your bio. The image, role, and bio are all customisable",
+    bio: "This is your bio boib. The image, role, and bio are all customisable",
     email: "darshanarlsingh@gmail.com",
     linkedin: "",
     github: "",
@@ -68,7 +68,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 600, // revalidate time for [slug], index
+  revalidateTime: 300, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
--- src/routes/Feed/FeedHeader/CategorySelect.tsx
+++ src/routes/Feed/FeedHeader/CategorySelect.tsx
@@ -26,7 +26,7 @@ const CategorySelect: React.FC<Props> = () => {
   return (
     <StyledWrapper>
       <div ref={dropdownRef} className="wrapper" onClick={handleOpen}>
-        {currentCategory} Posts <MdExpandMore />
+        {currentCategory}  <MdExpandMore />
       </div>
       {opened && (
         <div className="content">
